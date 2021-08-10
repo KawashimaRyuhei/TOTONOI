106634168f45b007e475df593fc35897d357e5b1
KawashimaRyuhei <11011011wxy@gmail.com>
2021-08-10 17:59:13 +0900

    storeコントローラーと関連するviewの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'stores/index'
+  get 'stores/show'
+  get 'stores/new'
+  get 'stores/edit'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
