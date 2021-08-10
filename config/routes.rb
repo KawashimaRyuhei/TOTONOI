b434c301dd1a5a5175527b99adb8c16c10590c26
KawashimaRyuhei <11011011wxy@gmail.com>
2021-08-10 18:24:34 +0900

    トップページへのルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'stores/index'
-  get 'stores/show'
-  get 'stores/new'
-  get 'stores/edit'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "stores#index"
 end
