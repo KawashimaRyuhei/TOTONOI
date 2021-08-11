c02f40750aeb4d5e5b31211b442fed439b04246d
KawashimaRyuhei <11011011wxy@gmail.com>
2021-08-11 14:29:01 +0900

    userモデルのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname
+      
+    with_options length: { minimum: 6 },
+      format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/,
+               message: 'Please include both letters and numbers in your settings' } do
+      validates :encrypted_password
+      validates :password
+      validates :password_confirmation
+    end
+  end  
 end
