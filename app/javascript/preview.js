719243b09ea7a38c53da89d5bca6d424659710f8
KawashimaRyuhei <11011011wxy@gmail.com>
2021-08-17 15:58:28 +0900

    画像プレビュー機能の追加

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
-  console.log(good);
+  if (document.getElementById('store_image')){
+    const ImageList = document.getElementById('image-list');
+    
+    document.getElementById('store_image').addEventListener('change', function(e){
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      const imageElement = document.createElement('div');
+
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    });
+  };
 });
