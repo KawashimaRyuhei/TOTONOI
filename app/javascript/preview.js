458bac8909a61b913cc8e542ba1bb482f3bba7eb
KawashimaRyuhei <11011011wxy@gmail.com>
2021-08-17 16:26:56 +0900

    preview.jsのリファクタリング

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -2,6 +2,16 @@ document.addEventListener('DOMContentLoaded', function(){
   if (document.getElementById('store_image')){
     const ImageList = document.getElementById('image-list');
     
+    const createImageHTML = (blob) => {
+      const imageElement = document.createElement('div');
+
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    };
+
     document.getElementById('store_image').addEventListener('change', function(e){
       const imageContent = document.querySelector('img');
       if (imageContent){
@@ -11,13 +21,7 @@ document.addEventListener('DOMContentLoaded', function(){
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
 
-      const imageElement = document.createElement('div');
-
-      const blobImage = document.createElement('img');
-      blobImage.setAttribute('src', blob);
-
-      imageElement.appendChild(blobImage);
-      ImageList.appendChild(imageElement);
+      createImageHTML(blob);
     });
   };
 });
