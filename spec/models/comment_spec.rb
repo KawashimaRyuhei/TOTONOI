e1994350f8c671bc6d9fca3207cb2806fb85cc11
KawashimaRyuhei <11011011wxy@gmail.com>
2021-09-02 11:04:37 +0900

    commentモデルの単体テスト実行

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe do
+    before do
+      @content = FactoryBot.build(:comment)
+    end
+  
+    context 'コメントが投稿できる' do
+      it 'contentが入力されていればコメント投稿できる' do
+        expect(@content).to be_valid
+      end
+    end
+
+    context 'コメントが投稿できない' do
+      it 'contentが空だと投稿できない' do
+        @content.content = ''
+        @content.valid?
+        expect(@content.errors.full_messages).to include("Content can't be blank")
+      end
+
+      it 'userが紐づいていないと投稿できない' do
+        @content.user = nil
+        @content.valid?
+        expect(@content.errors.full_messages).to include("User must exist")
+      end
+
+      it 'storeが紐づいていないと投稿できない' do
+        @content.store = nil
+        @content.valid?
+        expect(@content.errors.full_messages).to include("Store must exist")
+      end
+    end
+  end
 end
