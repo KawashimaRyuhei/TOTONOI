75086ad4db5e56c8914415a9cb645144f156a85c
KawashimaRyuhei <11011011wxy@gmail.com>
2021-09-06 13:30:20 +0900

    結合テストコード（user登録）

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+def basic_pass(path)
+  username = ENV['BASIC_AUTH_USER']
+  password = ENV['BASIC_AUTH_PASSWORD']
+  visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
+end
+
+RSpec.describe "Users", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  
+  describe "GET #index" do
+    it '正しい情報を入力すればユーザーの新規登録ができトップページに移動する' do
+      # 登録されているBasic認証のIDとPWで通過できること
+      basic_pass root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ遷移する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'user[nickname]', with: @user.nickname
+      fill_in 'user[email]', with: @user.email
+      fill_in 'user[password]', with: @user.password 
+      fill_in 'user[password_confirmation]', with: @user.password_confirmation
+      # 新規登録ボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change {User.count}.by(1)
+      # トップページへ遷移したことを確認する
+      expect(current_path).to eq(root_path)
+      # サインアップページやログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('ログイン')
+      expect(page).to have_no_content('新規登録')
+    end
+  end
+end
