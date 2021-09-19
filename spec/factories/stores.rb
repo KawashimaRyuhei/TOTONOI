f3660d0eae9656a32f23e08efa1b76bd661f4c88
KawashimaRyuhei <11011011wxy@gmail.com>
2021-09-19 20:14:05 +0900

    FactoryBot(stores)のwordsからwordに置換

--- spec/factories/stores.rb
+++ spec/factories/stores.rb
@@ -1,26 +1,26 @@
 FactoryBot.define do
   factory :store do
-    name { Faker::Lorem.words }
+    name { Faker::Lorem.word }
     address { Faker::Address.city }
     postal_code { Faker::Address.street_address }
     telephone { Faker::PhoneNumber.cell_phone }
     url { Faker::Internet.url }
-    closing_day { Faker::Lorem.words }
+    closing_day { Faker::Lorem.word }
     business_hour { Faker::Number.within(range: 1..24) }
     fee { Faker::Number.within(range: 1..10_000) }
-    parking_car { Faker::Lorem.words }
-    parking_bicycle { Faker::Lorem.words }
-    towel_rental { Faker::Lorem.words }
+    parking_car { Faker::Lorem.word }
+    parking_bicycle { Faker::Lorem.word }
+    towel_rental { Faker::Lorem.word }
     water { Faker::Number.within(range: 1..100) }
     water_depth { Faker::Number.within(range: 1..100) }
-    water_quality { Faker::Lorem.words }
+    water_quality { Faker::Lorem.word }
     temperature { Faker::Number.within(range: 1..100) }
-    roryu_status { Faker::Lorem.words }
+    roryu_status { Faker::Lorem.word }
     roryu_time { Faker::Number.within(range: 1..24) }
-    air_bath { Faker::Lorem.words }
-    break_place { Faker::Lorem.words }
-    television { Faker::Lorem.words }
-    bgm { Faker::Lorem.words }
+    air_bath { Faker::Lorem.word }
+    break_place { Faker::Lorem.word }
+    television { Faker::Lorem.word }
+    bgm { Faker::Lorem.word }
     association :user
   end
 end
